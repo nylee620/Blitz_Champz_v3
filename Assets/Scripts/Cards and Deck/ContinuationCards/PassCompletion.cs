94840abe3ad951ac2b595c07ccedc24816399c02
absweileh <bsweileh@memphis.edu>
2021-11-12 15:24:52 -0600

    Fixing First Down card
    Close #9
    First Down card provides draw 2 card for user and add them to the player hand.
    
    Acceptance Criteria:
    When a player play First Down card the computer should provide the user with 2 additional card from the deck

--- Assets/Scripts/Cards and Deck/ContinuationCards/PassCompletion.cs
+++ Assets/Scripts/Cards and Deck/ContinuationCards/PassCompletion.cs
@@ -18,17 +18,14 @@ public class PassCompletion : Card
 
     public new void Play()
     {
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Table.myGamePlayer.myTurn)
+        {
+            if (Table.myGamePlayer.table.homeLastPlayed.transform.childCount > 0)
+            {
+                Table.myGamePlayer.table.homeLastPlayed.transform.GetChild(0).transform.SetParent(Table.myGamePlayer.table.homePlayedCards.transform);
+            }
+            this.transform.SetParent(Table.myGamePlayer.table.homeLastPlayed.transform);
+            Table.myGamePlayer.table.DrawCard(2);
+        }
     }
 }
